3d53e2913ee7b14e70db4c8a9a0ea996caec2970
designatedcoder <83379213+designatedcoder@users.noreply.github.com>
2025-08-10 15:57:10 -0400

    first folio dashboard page w/layout file

---
+++ resources/views/pages/dashboard.blade.php
@@ -0,0 +1,30 @@
+<?php
+    use function Laravel\Folio\Name;
+
+    name('dashboard');
+?>
+
+@extends('layouts.app')
+
+@section('title', 'Dashboard')
+
+@section('content')
+    <div class="flex flex-col space-y-6 p-4 sm:ml-64">
+        <div>
+            <h2 class="text-2xl">Dashboard</h2>
+        </div>
+        <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis vero explicabo quam hic dicta porro blanditiis qui cum, commodi fugit doloremque quasi nesciunt odio, sunt facere culpa eius facilis rerum!
+        </p>
+        <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis vero explicabo quam hic dicta porro blanditiis qui cum, commodi fugit doloremque quasi nesciunt odio, sunt facere culpa eius facilis rerum!
+        </p>
+        <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis vero explicabo quam hic dicta porro blanditiis qui cum, commodi fugit doloremque quasi nesciunt odio, sunt facere culpa eius facilis rerum!
+        </p>
+        <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis vero explicabo quam hic dicta porro blanditiis qui cum, commodi fugit doloremque quasi nesciunt odio, sunt facere culpa eius facilis rerum!
+        </p>
+    </div>
+@endsection
+
