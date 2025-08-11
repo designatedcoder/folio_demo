cefd821266623e5c1824b46eee71d5a3b66e0228
designatedcoder <83379213+designatedcoder@users.noreply.github.com>
2025-08-10 21:13:39 -0400

    fixed missing style tags on auth layout and show page

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -26,10 +26,13 @@
             <div
                 class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
                 <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-                    @yield('header')
+                    <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                        @yield('header')
+                    </h1>
                     @yield('form')
                     @yield('footer')
                 </div>
+            </div>
         </div>
     </section>
 </body>
--- resources/views/pages/admin/users/[User].blade.php
+++ resources/views/pages/admin/users/[User].blade.php
@@ -27,7 +27,7 @@
                 <div class="mb-5">
                     <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">E-mail</label>
                     <input type="email" id="email" name="email" value="{{ $user->email }}" class="shadow-xs bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-xs-light" placeholder="name@flowbite.com" />
-                    @error('name')
+                    @error('email')
                         <div class="text-red-600">{{ $message }}</div>
                     @enderror
                 </div>
